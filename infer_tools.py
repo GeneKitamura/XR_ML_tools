d5ee80427985d2210088c86c2a307f432fa965af
Genesis <8467955+Razorocean@users.noreply.github.com>
2020-11-01 13:01:58 -0500

    ensemble

--- infer_tools.py
+++ infer_tools.py
@@ -1,6 +1,9 @@
 import numpy as np
 import skimage
 import math
+import itertools
+
+from scipy import stats
 
 def np_preprocess(uint16=False):
     if not uint16:
@@ -59,3 +62,80 @@ def label_with_cat_model(npz_path, model_weights, load_model, uint16=True, n_cla
         np.savez(file_name, image_array=image_array, index_array=index_array, label_array=labels)
 
     return image_array, index_array, labels
+
+def ensemble_scalar_outs(scalar_list=None, params=None, path_val=None, diff_pvals=True, return_best=False):
+    if scalar_list is None:
+        scalar_list = [20, 40, 60, 80, 100, 120, 140, 160, 180]
+
+    if path_val is None:
+        path_val = './rot/test_vals/{}'
+
+    if params is None:
+        params = {
+            'labels': 'orig_labels',
+            'predictions': 'scaled_pred_angle'
+        }
+
+    combs = list(itertools.combinations(scalar_list, 2))
+    str_labels = params['labels']
+    str_preds = params['predictions']
+
+    #TODO: best_val=None
+    for i in scalar_list:
+        one_path = path_val.format(i)
+        with np.load(one_path + '.npz') as f:
+            one_labels = f[str_labels]
+            one_preds = f[str_preds]
+
+        diff = np.abs(one_labels - one_preds)
+        mean_diff = np.mean(diff)
+        ci = 1.96 * np.std(diff) / np.sqrt(diff.shape[0])
+        print('{} MAE: {:.2f} +- {:.2f}'.format(i, mean_diff, ci))
+
+    print('\n')
+    for i, j in combs:
+        one_path = path_val.format(i)
+        with np.load(one_path + '.npz') as f:
+            one_labels = f[str_labels]
+            one_preds = f[str_preds]
+
+        two_path = path_val.format(j)
+        with np.load(two_path + '.npz') as f:
+            two_labels = f[str_labels]
+            two_preds = f[str_preds]
+
+        one_diff = np.abs(one_labels - one_preds)
+        two_diff = np.abs(two_labels - two_preds)
+        if diff_pvals:
+            p_val = stats.ttest_rel(one_diff, two_diff).pvalue
+        else:
+            p_val = stats.ttest_rel(one_preds, two_preds).pvalue
+        print('curr vals {} and {} with p_val {:.4f}'.format(i, j, p_val))
+
+        mean_vals = (one_preds + two_preds) / 2
+        max_vals = np.maximum(one_preds, two_preds)
+        min_vals = np.minimum(one_preds, two_preds)
+
+        _stack_vals = np.stack([one_preds, two_preds], axis=1)
+        _abs_max_ind = np.argmax(np.abs(_stack_vals), axis=1)
+        abs_max_vals = np.where(_abs_max_ind == 0, one_preds, two_preds)
+        abs_min_vals = np.where(_abs_max_ind == 0, two_preds, one_preds)  # flipped
+
+        vals_mix_list = [mean_vals, max_vals, min_vals, abs_max_vals, abs_min_vals]
+        mix_names = ['mean', 'max', 'min', 'abs_max', 'abs_min']
+
+        assert np.mean(one_labels - two_labels) == 0, 'labels means different'
+
+        for mix_val, mix_name in zip(vals_mix_list, mix_names):
+            mix_diff = np.abs(one_labels - mix_val)
+            if diff_pvals:
+                one_p_val = stats.ttest_rel(one_diff, mix_diff).pvalue
+                two_p_val = stats.ttest_rel(two_diff, mix_diff).pvalue
+            else:
+                one_p_val = stats.ttest_rel(one_preds, mix_val).pvalue
+                two_p_val = stats.ttest_rel(two_preds, mix_val).pvalue
+            mean_diff = np.mean(mix_diff)
+            ci = 1.96 * np.std(mix_diff) / np.sqrt(mix_diff.shape[0])
+            print('{} MAE: {:.2f} +- {:.2f}. one_p_val {:.4f}, two_p_val {:.4f}'.format(mix_name, mean_diff, ci, one_p_val, two_p_val))
+
+        print('\n')
